6689ac0ac4728de1f7ae00b6c47821ffc012ddd2
Ranjan Kumar <kumar.ranjan@sasken.com>
2025-05-23 10:49:41 +0530

    feat: add Postgres via Docker and run initial migration
    - Added docker-compose config for Postgres setup
    - Performed initial DB schema migration

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
-use post_notify::run;
+use post_notify::configuration::get_configuration;
+use post_notify::startup::run;
+use sqlx::PgPool;
 use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:8000")?;
-    run(listener)?.await
+    dotenvy::dotenv().ok();
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
+    run(listener, connection_pool)?.await
 }
